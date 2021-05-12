b12d11bc4cb0634078bb5612f54a2d29c51100c7
shufan xia <sxia1@haverford.edu>
2021-05-12 14:20:06 -0400

    lambda function

--- SEQ.py
+++ SEQ.py
@@ -9,6 +9,16 @@ plt.rc('text',usetex =True)
 
 
 # In[24]:
+### some paramters from adapted the data on Figure 4.3, Townsend
+g = 2 # g factor of electron
+q = 4.803207 *10**(-10) #  esu Gaussian units
+m = 1.883533* 10**(-25)  # g
+c = 3.0*10**10 #cm/s
+B0 = 60 #gauss
+B1 = 6  #gauss
+w00 = g*q/(2*m*c)*B0 
+w10 = g*q/(2*m*c)*B1
+T1 = 2*np.pi/w10 #12 microscond 12*10^-6, period of transition if w1 =  w10, 80kHz
 
 
 def f(Psi,t,params):
@@ -20,8 +30,8 @@ def f(Psi,t,params):
     aI = Psi[1]
     bR = Psi[2]
     bI = Psi[3]
-    daR = 1/2*(w0*aI+w1*np.cos(w*t)*bI)
-    daI = -1/2*(w0*aR+w1*np.cos(w*t)*bR)
+    daR = 1/2*(w0*aI+w1*np.cos(w*t)*bI) # Real part of the derivative a
+    daI = -1/2*(w0*aR+w1*np.cos(w*t)*bR) # Imaginary part of the derivative a
     dbR =  1/2*(w1*np.cos(w*t)*aI-w0*bI)
     dbI =  -1/2*(w1*np.cos(w*t)*aR-w0*bR)
     
@@ -35,30 +45,19 @@ def f(Psi,t,params):
     #           P_dowm: an array of the probability of getting -z when we measure S_z at each time step
     #           transition_rate: the proportion of particles that transtion from +z to -z, defined as the maximum value of P_down.
 def P_z(w0,w1,w,r0,N):
-    t = np.linspace(0,3*T1,N) #time step from t=0 to t= 3T1
+    t = np.linspace(0,2*T1,N) #time step from t=0 to t= 2T1
     params = [w0,w1,w] 
     psoln = odeint(f, r0, t, args=(params,)) # solve by odeint
     aR = psoln[:,0]
     aI = psoln[:,1]
     bR = psoln[:,2]
     bI = psoln[:,3]
-    P_up = list(map(lambda r,i: abs(r+i*1j)**2,aR,aI))    #getting P_up through the use of a lambda function to cut down on the use of for-loops
-    P_down = list(map(lambda r,i: abs(r+i*1j)**2,bR,bI))  #getting P_down through the use of a lambda function to cut down on the use of for-loops
+    P_up = list(map(lambda r,i: abs(r+i*1j)**2,aR,aI))    # probablitly of measuring the spin-up: the magnitude square of the coefficent on |z+>
+    P_down = list(map(lambda r,i: abs(r+i*1j)**2,bR,bI))  # 
     transition_rate = np.max(P_down)                      #defining as the maximum value of P_down as mentioned above
     return t,P_up,P_down,transition_rate
 
 
-### some paramters from adapted the data on Figure 4.3, Townsend
-hbar = 4.135667696*10**-15
-g = 2
-q = 4.803207 *10**(-10) # in Gaussian units
-m = 1.883533* 10**(-25)  
-c = 3.0*10**10 #cm/s
-B0 = 60
-B1 = 6
-w00 = g*q/(2*m*c)*B0
-w1 = g*q/(2*m*c)*B1
-T1 = 2*np.pi/w1 #12 microscond 12*10^-6
 
 
 # ### At resonance, and a weak oscilating field
@@ -73,8 +72,9 @@ xtick_name = (xtic0, xtic1, xtic2, xtic3, xtic4) #convert the x-axis tick to uni
 # In[101]:
 
 
-r0 = [1.0,0.,0.,0.]
-w0,w1,w = 1.0,0.1,1.0
+r0 = [1.0,0.,0.,0.] #initial condition, spin up: {{1+0j},{0+0j}}
+
+w0,w1,w = 1.0,0.1,1.0 #
 
 f0,f1,f_ac = np.array([w0,w1, w])*800 #kHz for plot title, with f0 as the frequency for ω0 and f1 as the frequency for ω1. f_ac is for the alternating current signal
 t,P_up,P_down,transition_rate = P_z(1.0,0.1,1.0,r0,600) #600 time step
@@ -96,7 +96,7 @@ ax.set_xticklabels(xtick_name,fontsize=18)
 ax.grid(True)
 
 transition_rate = np.max(P_down)
-ax.annotate("Transition rate = %0.2f"%transition_rate, (0.65,0.76),xycoords = "figure fraction", fontsize =20)
+ax.annotate("Transition rate = %0.2f"%transition_rate, (0.66,0.685),xycoords = "figure fraction", fontsize =20)
 
 # Create axes for sliders
 ax_w0 = fig.add_axes([0.3, 0.22, 0.4, 0.03])
@@ -113,7 +113,7 @@ ax_t.spines['top'].set_visible(True)
 ax_t.spines['right'].set_visible(True)
 # craet three sliders
 w0_slider = Slider(ax=ax_w0,label='$\omega_0$',valmin=0.,valmax=2.,valinit=1.0,facecolor='#cc7000',dragging= True)
-w1_slider = Slider(ax=ax_w1,label='$\omega_1$',valmin=0.,valmax=2.,valinit=0.1,facecolor='#cc7000',dragging= True)
+w1_slider = Slider(ax=ax_w1,label='$\omega_1$',valmin=0.,valmax=0.5,valinit=0.1,facecolor='#cc7000',dragging= True)
 w_slider = Slider(ax=ax_w,label='$\omega$',valmin=0.,valmax=2.,valinit=1.0,facecolor='#cc7000',dragging= True)
 t_slider = Slider(ax=ax_t,label='N steps',valmin=100.,valmax=1000.,valfmt='%0.0f',valinit=600,facecolor='#cc7000',dragging= True)
 
@@ -157,7 +157,7 @@ def trans_rate_exp(w0,w1,w):
 r0 = [1.,0.,0.,0.]
 w0,w1 = 1.,0.1
 f0,f1= np.array([w0,w1])*800 #kHZ for plot title
-w_list = np.arange(0.6,1.4,0.005)
+w_list = np.arange(0.4,1.4,0.005)
 rate = np.array(list(map(lambda w: P_z(w0,w1,w,r0,600), w_list)))[:,3]
 
 fig= plt.figure(figsize=(15,13))
@@ -184,7 +184,7 @@ ax_t.spines['top'].set_visible(True)
 ax_t.spines['right'].set_visible(True)
 
 w0_slider = Slider(ax=ax_w0,label='$\omega_0$',valmin=0.,valmax=2.,valinit=1.0, facecolor='#cc7000',dragging= True)
-w1_slider = Slider(ax=ax_w1,label='$\omega_1$',valmin=0.,valmax=1.,valinit=0.1, facecolor='#cc7000',dragging= True)
+w1_slider = Slider(ax=ax_w1,label='$\omega_1$',valmin=0.,valmax=1.,valinit=0.4, facecolor='#cc7000',dragging= True)
 t_slider = Slider(ax=ax_t,label='N steps',valmin=100.,valmax=1000.,valfmt='%0.0f',valinit=600,facecolor='#cc7000',dragging= True)
 
 # Update values
